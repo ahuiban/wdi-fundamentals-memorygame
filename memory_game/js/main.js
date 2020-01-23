ee821489df5672488969064aed2f3ca6d24d7f5e
Andrei <37360070+ahuiban@users.noreply.github.com>
2020-01-23 10:33:41 -0500

    Memory Game - Software Engineering Immersive

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,9 +2,9 @@
 console.log("Up and running!");
 var cards = [
    {
-       rank: "queen",
-       suit: "hearts",
-       cardImage: "images/queen-of-hearts.png",
+      rank: "queen",
+      suit: "hearts",
+      cardImage: "images/queen-of-hearts.png",
    },
    {
       rank: "queen",
@@ -13,56 +13,56 @@ var cards = [
    },
 
    {
-    rank: "king",
-    suit: "hearts",
-    cardImage: "images/king-of-hearts.png",
+      rank: "king",
+      suit: "hearts",
+      cardImage: "images/king-of-hearts.png",
    },
-    {
-    rank: "king",
-    suit: "diamonds",
-    cardImage: "images/king-of-diamonds.png",
+   {
+      rank: "king",
+      suit: "diamonds",
+      cardImage: "images/king-of-diamonds.png",
    },
 ];
 
 var cardsInPlay = [];
 
- function checkForMatch() {
-  
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-          alert("You found a match!");
+function checkForMatch() {
 
-      } else {
-          alert("Sorry, try again");
-        };
+   if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
 
-  };
+   } else {
+      alert("Sorry, try again");
+   };
+
+};
 
 
 function flipCard() {
-    var cardId = this.getAttribute('data-id');
- console.log("User flipped" + cards[cardId].rank);
-  cardsInPlay.push(cards[cardId].rank);
- this.setAttribute('src', cards[cardId].cardImage);
+   var cardId = this.getAttribute('data-id');
+   console.log("User flipped" + cards[cardId].rank);
+   cardsInPlay.push(cards[cardId].rank);
+   this.setAttribute('src', cards[cardId].cardImage);
 
- if (cardsInPlay.length === 2){
-    checkForMatch();
-    
+   if (cardsInPlay.length === 2) {
+      checkForMatch();
 
- };
+
+   };
 };
 
 
 
- function createBoard() {
-  var cardTable = document.getElementById('game-board');
-  for (var i = 0; i < cards.length; i++) {
-  var cardElement = document.createElement('img');
-    cardElement.setAttribute('src', 'images/back.png');
-    cardElement.setAttribute('data-id', i);
-    cardElement.addEventListener('click', flipCard);
-    cardTable.appendChild(cardElement);
+function createBoard() {
+   var cardTable = document.getElementById('game-board');
+   for (var i = 0; i < cards.length; i++) {
+      var cardElement = document.createElement('img');
+      cardElement.setAttribute('src', 'images/back.png');
+      cardElement.setAttribute('data-id', i);
+      cardElement.addEventListener('click', flipCard);
+      cardTable.appendChild(cardElement);
 
-  }
+   }
 };
 
 createBoard();
